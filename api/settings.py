aabf4898b89edaa71998a6a776888088ff2b32ba
Jesse <iamjesse75@gmail.com>
2022-10-06 14:55:32 +0000

    added cors_origin_whitelist

--- api/settings.py
+++ api/settings.py
@@ -29,7 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'localhost']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+
 
 REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
 
