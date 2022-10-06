85cf87aa2e5d4f89f9728e55f00591e73a58bf1e
Jesse <iamjesse75@gmail.com>
2022-10-06 16:00:04 +0000

    added swagger documentation

--- api/urls.py
+++ api/urls.py
@@ -20,8 +20,17 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework_swagger.views import get_swagger_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
-# schema_view = get_swagger_view(openapi.Info(title="API Documentation", default_version='v1'))
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
@@ -30,5 +39,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     # path('docs/', include_docs_urls(title="Sassty API documentation")),
     # path('docs/', schema_view)
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
+
 ]
 
