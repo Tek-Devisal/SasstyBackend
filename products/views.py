7c5349bfd30db33f43e3c1d6a502aa1198de8d86
Jesse <iamjesse75@gmail.com>
2022-11-03 19:40:46 +0000

    permision class added to fetchCategories API

--- products/views.py
+++ products/views.py
@@ -2,18 +2,17 @@ from .models import Categories, Products, SubCategories
 from products.serializers import CategorySerializer, ProductSerializer, SubCategorySerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import permissions
-
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from random import shuffle
 
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny
  
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def fetchCategories(request, format=None):
     if request.method == 'GET':
-        permission_classes = [AllowAny]
-        permission_classes.__add__
+       
         #get all categories
         categories = Categories.objects.all()
 
